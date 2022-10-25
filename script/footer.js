d99632a134d7e4aeb3c8500198e2385d68743373
tonybare <90167174+tonybare@users.noreply.github.com>
2022-10-25 20:06:33 +0800

    Add files via upload

---
+++ script/footer.js
@@ -0,0 +1,12 @@
+footer = `<div class="footer"">
+<div class="container-fluid fixed-bottom" style="background-color: black;>
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <p class="mbr-text mb-0 mbr-fonts-style display-7" style="color: aliceblue;">
+            © Copyright 2022 NPTU CSIE - All Rights Reserved
+            </p>
+        </div>
+    </div>
+</div>
+</div>`;
+document.write(footer);
